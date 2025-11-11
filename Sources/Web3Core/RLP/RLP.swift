2822d4bfd1a5cec44fa0b9b0a0c210d69e16bb9c
Chuanqing <15318119557@163.com>
2025-11-11 14:37:14 +0800

    Update RLP.swift

--- Sources/Web3Core/RLP/RLP.swift
+++ Sources/Web3Core/RLP/RLP.swift
@@ -111,7 +111,7 @@ public struct RLP {
         return encoded.bytesArray[0]
     }
 
-    internal static func encode(_ elements: [Any?]) -> Data? {
+    public static func encode(_ elements: [Any?]) -> Data? {
         var encodedData = Data()
         for e in elements {
             if let encoded = encode(element: e) {
